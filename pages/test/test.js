947456251d7f50d847c7c9854783f8fc10880f4c
sky198719 <sky198719@163.com>
2021-03-05 17:29:45 +0800

    test

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,13 +1,3 @@
-const global = require('../../utils/global.js')
-
 Page({
-  data:{
-    currentTab:1
-  },
-  changeTab(e){
-    this.setData({
-      currentTab:e.currentTarget.dataset.tabid
-    })
-    console.log(this.data.currentTab)
-  }
+
 })
